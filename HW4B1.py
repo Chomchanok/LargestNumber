eb9073e988dbc0f2ee921880e9c14fd0d9766b5c
Chomchanok <chomchanok_y@cmu.ac.th>
2021-07-31 20:38:48 +0700

    add comments

--- HW4B1.py
+++ HW4B1.py
@@ -1,22 +1,20 @@
-import itertools 
-  
-listA =  [10,1]##[121, 1, 2, 50]#[50,1,2,9]
-perm = itertools.permutations(listA) 
-newlist = [] 
+import itertools  # using combinatoric iterators
+listA =  [50,1,2,9]#[10,1]#[121, 1, 2, 50]#[50,1,2,9]
+perm = itertools.permutations(listA) #using permutation to arrange number in listA
+newlist = [] #create newlist to collect number after rearrangement
 for i in list(perm): 
-    arrayConcat = []
-    stringC = ""
+    arrayConcat = [] # create arrayConcat list to collect every posible number 
+    stringC = "" # create stringC to merge every number in arrayConcat list and tranform to string type
     #print(i)
     for j in i:
-        arrayConcat.append(str(j))
-    #print(j)
-    for i in arrayConcat:
-        stringC += i 
-    #print(stringC)
-      
-    for i in stringC:
+        arrayConcat.append(str(j)) # collect every possible patterns
+    #print(arrayConcat)
+    for key in arrayConcat:
+        stringC += key # merge number in list to string
+    print(stringC)   
+    for s in stringC: 
         newlist.append(stringC)
-print(max(newlist))
+print(max(newlist)) # finding max value in newlist and show output
     
          
                 
